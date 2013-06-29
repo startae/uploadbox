de0d990005a727d84c8ed93e2842e258b177a78e
Julio Protzek <julioprotzek@gmail.com>
2013-06-29 12:16:30 -0300

    add carrierwave initializer

--- lib/generators/file_uploader/image/image_generator.rb
+++ lib/generators/file_uploader/image/image_generator.rb
@@ -4,6 +4,10 @@ module FileUploader
 
     source_root File.expand_path('../templates', __FILE__)
 
+    def create_initializer
+      copy_file 'initializers/carrierwave.rb', 'config/initializers/carrierwave.rb'
+    end
+
     def create_uploader
       copy_file 'uploaders/image_uploader.rb', 'app/uploaders/image_uploader.rb'
     end
@@ -13,16 +17,17 @@ module FileUploader
     end
 
     def create_migration
-      migration_template 'migrate/create_images.rb', "db/migrate/create_images.rb"
+      migration_template 'migrate/create_images.rb', 'db/migrate/create_images.rb'
     end
+
 
     private
 
     def self.next_migration_number(dirname)
       if ActiveRecord::Base.timestamped_migrations
-        Time.now.utc.strftime("%Y%m%d%H%M%S")
+        Time.now.utc.strftime('%Y%m%d%H%M%S')
       else
-        "%.3d" % (current_migration_number(dirname) + 1)
+        '%.3d' % (current_migration_number(dirname) + 1)
       end
     end
   end
