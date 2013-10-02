5b98e82599b73e12180271cbe6b1deb7624a9110
Julio Protzek <julioprotzek@gmail.com>
2013-10-02 19:53:34 -0300

    bufixes

--- app/helpers/uploadbox/img_helper.rb
+++ app/helpers/uploadbox/img_helper.rb
@@ -1,5 +1,20 @@
 module Uploadbox
   module ImgHelper
+    def s3_policy
+      Base64.encode64(policy_data.to_json).gsub("\n", "")
+    end
+
+
+    def s3_signature
+      Base64.encode64(
+        OpenSSL::HMAC.digest(
+          OpenSSL::Digest::Digest.new('sha1'),
+          ENV['S3_SECRET'],
+          s3_policy
+        )
+      ).gsub("\n", "")
+    end
+
     def img(*args)
       upload = args[0]
       if upload.is_a? CarrierWave::Uploader::Base
@@ -8,5 +23,19 @@ module Uploadbox
         image_tag(*args)
       end
     end
+
+    private
+      def policy_data
+        {
+          expiration: 10.hours.from_now.utc.iso8601,
+          conditions: [
+            ["starts-with", "$key", 'uploads/'],
+            ["content-length-range", 0, 500.megabytes],
+            ["starts-with","$content-type",""],
+            {bucket: ENV['S3_BUCKET']},
+            {acl: 'public-read'}
+          ]
+        }
+      end
   end
 end
