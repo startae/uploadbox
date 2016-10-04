6aa0e221399cfb9e5e2f07447e257cff10c3ec20
Bruno Arueira <contato@brunoarueira.com>
2016-10-04 09:50:24 -0300

    Add config to infer the spec type

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -28,6 +28,8 @@ RSpec.configure do |config|
   config.infer_base_class_for_anonymous_controllers = false
   config.order = 'random'
 
+  config.infer_spec_type_from_file_location!
+
   config.before(:suite) do
     DatabaseCleaner.strategy = :truncation
     DatabaseCleaner.clean_with(:truncation)
